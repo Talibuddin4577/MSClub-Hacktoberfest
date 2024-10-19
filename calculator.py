e295bbeaad97563adb56b27471c1bb1e6a9e8588
Talibuddin4577 <talibuddinahmedtauseef@gmail.com>
2024-10-19 15:05:26 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,4 +1,5 @@
-# calculator.py
+import math
+
 def add(a, b):
     return a + b
 
@@ -12,6 +13,14 @@ def divide(a, b):
     if b == 0:
         return "Cannot divide by zero"
     return a / b
+
+def power(a, b):
+    return a ** b
+
+def square_root(a):
+    if a < 0:
+        return "Cannot take square root of a negative number"
+    return math.sqrt(a)
 
 if __name__ == "__main__":
     print("Basic Calculator is ready!")
